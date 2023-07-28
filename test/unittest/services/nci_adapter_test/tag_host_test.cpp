b8e26c60241c81cbd2b57dcd2abe8ff56fe8f889
hefeiyue <hefeiyue@h-partners.com>
2023-07-28 18:08:39 +0800

    TDD覆盖tag_host.cpp
    Signed-off-by: hefeiyue <hefeiyue@h-partners.com>

---
+++ test/unittest/services/nci_adapter_test/tag_host_test.cpp
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <gtest/gtest.h>
+#include <thread>
+
+#include "tag_host.h"
+#include "nfc_service.h"
+
+namespace OHOS {
+namespace NFC {
+namespace TEST {
+using namespace testing::ext;
+using namespace OHOS::NFC::NCI;
+
+std::vector<int> tagTechList = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+std::vector<int> tagRfDiscIdList = {0, 1, 2};
+std::vector<int> tagActivatedProtocols = {0x04};
+std::string tagUid = "5B7FCFA9";
+std::vector<std::string> tagPollBytes = {"00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0A", "0B",
+    "0C", "0D", "0E", "0F", "10", "11"};
+std::vector<std::string> tagActivatedBytes = tagPollBytes;
+
+class TagHostTest : public testing::Test {
+public:
+    static void SetUpTestCase() {}
+    static void TearDownTestCase() {}
+    void SetUp();
+    void TearDown();
+
+    std::shared_ptr<NCI::ITagHost> tag_;
+};
+
+void TagHostTest::SetUp()
+{
+    tag_ = std::make_shared<TagHost>(
+        tagTechList, tagRfDiscIdList, tagActivatedProtocols, tagUid, tagPollBytes, tagActivatedBytes);
+    std::shared_ptr<NCI::ITagHost> tag = std::make_shared<TagHost>(
+        tagTechList, tagRfDiscIdList, tagActivatedProtocols, tagUid, tagPollBytes, tagActivatedBytes);
+    tag = nullptr;
+}
+
+void TagHostTest::TearDown()
+{
+    tag_ = nullptr;
+}
+
+void callback1(int tagRfDiscId)
+{}
+
+/**
+ * @tc.name: ConnectTest001
+ * @tc.desc: Test Connect
+ * @tc.type: FUNC
+ */
+HWTEST_F(TagHostTest, ConnectTest001, TestSize.Level1)
+{
+    std::vector<int> techList = tag_->GetTechList();
+    EXPECT_FALSE(techList.empty());
+    EXPECT_FALSE(tag_->Connect(static_cast<int>(KITS::TagTechnology::NFC_A_TECH)));
+    EXPECT_FALSE(tag_->FieldOnCheckingThread());
+    EXPECT_FALSE(tag_->IsTagFieldOn());
+    std::string uid = tag_->GetTagUid();
+    EXPECT_STREQ(uid.c_str(), "5B7FCFA9");
+    std::string req = "9060000000";
+    std::string res = "";
+    tag_->Transceive(req, res);
+    EXPECT_STREQ(res.c_str(), "");
+    tag_->OffFieldChecking();
+    EXPECT_FALSE(tag_->Connect(-1));
+    EXPECT_FALSE(tag_->Disconnect());
+}
+
+/**
+ * @tc.name: RemoveTechTest001
+ * @tc.desc: Test RemoveTech
+ * @tc.type: FUNC
+ */
+HWTEST_F(TagHostTest, RemoveTechTest001, TestSize.Level1)
+{
+    static const int INVALID_VALUE = -1;
+    tag_->RemoveTech(INVALID_VALUE);
+    int tagRfDiscId = tag_->GetTagRfDiscId();
+    EXPECT_EQ(tagRfDiscId, 0);
+    int fieldOnCheckInterval = 125;
+    static std::function<void(int)> callback = std::bind(callback1, std::placeholders::_1);
+    tag_->OnFieldChecking(callback, fieldOnCheckInterval);
+    fieldOnCheckInterval = 0;
+    tag_->OnFieldChecking(callback, fieldOnCheckInterval);
+}
+
+/**
+ * @tc.name: GetTechExtrasDataTest001
+ * @tc.desc: Test GetTechExtrasData
+ * @tc.type: FUNC
+ */
+HWTEST_F(TagHostTest, GetTechExtrasDataTest001, TestSize.Level1)
+{
+    int tagRfDiscId = tag_->GetTagRfDiscId();
+    EXPECT_EQ(tagRfDiscId, 0);
+    std::vector<AppExecFwk::PacMap> tagTechExtras = tag_->GetTechExtrasData();
+    tagTechList.clear();
+    tagPollBytes.clear();
+    tagActivatedBytes.clear();
+    tag_ = std::make_shared<TagHost>(
+        tagTechList, tagRfDiscIdList, tagActivatedProtocols, tagUid, tagPollBytes, tagActivatedBytes);
+    tag_->GetTechExtrasData();
+    tagTechList = {1, 2, 3, 4, 5, 6};
+    tagPollBytes = {"00", "01", "02", "03", "04", "05", "06"};
+    tagActivatedBytes = tagPollBytes;
+    tag_ = std::make_shared<TagHost>(
+        tagTechList, tagRfDiscIdList, tagActivatedProtocols, tagUid, tagPollBytes, tagActivatedBytes);
+    tag_->GetTechExtrasData();
+    EXPECT_FALSE(tag_->GetConnectedTech() == static_cast<int>(KITS::TagTechnology::NFC_ISODEP_TECH));
+}
+
+/**
+ * @tc.name: ReadNdefTest001
+ * @tc.desc: Test ReadNdef
+ * @tc.type: FUNC
+ */
+HWTEST_F(TagHostTest, ReadNdefTest001, TestSize.Level1)
+{
+    std::string response = tag_->ReadNdef();
+    EXPECT_STREQ(response.c_str(), "");
+    std::string data = "";
+    EXPECT_FALSE(tag_->WriteNdef(data));
+    std::string key = "";
+    EXPECT_FALSE(tag_->FormatNdef(key));
+    key = "01";
+    EXPECT_FALSE(tag_->FormatNdef(key));
+    EXPECT_FALSE(tag_->IsNdefFormatable());
+    std::vector<int> ndefInfo;
+    EXPECT_FALSE(tag_->IsNdefMsgContained(ndefInfo));
+}
+}
+}
+}
