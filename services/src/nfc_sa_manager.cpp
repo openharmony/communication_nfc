48b117046b933fd0dec14dccb287d00346b6e5ec
amunra03 <liumanhao@huawei.com>
2024-08-19 15:34:53 +0800

    re subscribe common event after foundation crashed
    Signed-off-by: amunra03 <liumanhao@huawei.com>

--- services/src/nfc_sa_manager.cpp
+++ services/src/nfc_sa_manager.cpp
@@ -77,7 +77,20 @@ void NfcSaManager::OnAddSystemAbility(int32_t systemAbilityId, const std::string
     InfoLog("OnAddSystemAbility systemAbilityId:%{public}d added!", systemAbilityId);
     if (systemAbilityId != COMMON_EVENT_SERVICE_ID) {
         InfoLog("OnAddSystemAbility systemAbilityId is not COMMON_EVENT_SERVICE_ID");
+        return;
     }
+    InfoLog("Start to resubscribe common event.");
+    if (nfcService_ == nullptr) {
+        ErrorLog("nfcService_ is nullptr");
+        return;
+    }
+    if (nfcService_->eventHandler_ == nullptr) {
+        ErrorLog("eventHandler_ is nullptr");
+        return;
+    }
+    nfcService_->eventHandler_->SubscribePackageChangedEvent();
+    nfcService_->eventHandler_->SubscribeScreenChangedEvent();
+    nfcService_->eventHandler_->SubscribeShutdownEvent();
 }
 
 void NfcSaManager::OnRemoveSystemAbility(int32_t systemAbilityId, const std::string& deviceId)
