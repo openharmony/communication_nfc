182e9c9210c9074f8f24b2987dd874cfa76824c3
xzpm <2506327108@qq.com>
2025-03-03 21:11:54 +0800

    change sdk api version from 16 to 18
    Signed-off-by: xzpm <2506327108@qq.com>

--- frameworks/js/napi/common/nfc_napi_common_utils.cpp
+++ frameworks/js/napi/common/nfc_napi_common_utils.cpp
@@ -671,7 +671,7 @@ int BuildOutputErrorCode(int errCode)
     } else if (errCode == ERR_TAG_STATE_IO_FAILED) {
         return BUSI_ERR_IO_OPERATION_INVALID;
     } else if (errCode >= ERR_TAG_BASE && errCode < ERR_CE_BASE) {
-        if ((NfcSdkCommon::GetSdkVersion() >= SDK_VERSION_16) && (errCode == ERR_TAG_STATE_LOST)) {
+        if ((NfcSdkCommon::GetSdkVersion() >= SDK_VERSION_18) && (errCode == ERR_TAG_STATE_LOST)) {
             return BUSI_ERR_TAG_LEAVES_FIELD;
         }
         return BUSI_ERR_TAG_STATE_INVALID;
