fc9d3f108c0061badd632ce653a9edf023a6d5bc
fengyong <fengyong21h@-partners.com>
2025-04-25 15:49:28 +0800

    delete cj from bundle.json & clearing alams
    Signed-off-by: fengyong <fengyong21@h-partners.com>
    Signed-off-by: fengyong <fengyong21@h-partners.com>

--- frameworks/cj/cardEmulation/nfc_cardEmulation_ffi.cpp
+++ frameworks/cj/cardEmulation/nfc_cardEmulation_ffi.cpp
@@ -12,8 +12,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <cstring>
 
+#include <cstring>
 #include "cardEmulation.h"
 #include "loghelper.h"
 #include "hce_service.h"
