61f992219b0a88b024d0a48b4aa5a2e166b83a95
fengyong <fengyong21@h-partners.com>
2025-06-02 03:12:20 +0000

    update services/include/ndef_har_dispatch.h.
    Signed-off-by: fengyong <fengyong21@h-partners.com>

--- services/include/ndef_har_dispatch.h
+++ services/include/ndef_har_dispatch.h
@@ -43,8 +43,9 @@ public:
     bool DispatchByAppLinkMode(const std::string &uriSchemeValue, const std::shared_ptr<KITS::TagInfo> &tagInfo,
         OHOS::sptr<IRemoteObject> tagServiceIface);
 #ifdef NFC_HANDLE_SCREEN_LOCK
-    static uint64_t GetAlipayTime();
-    static void AbilityAlipay();    
+    void UnlockStartTimer();
+    static void UnlockStopTimer();
+    void UnlockTimerCallback();  
 #endif
 
 private:
