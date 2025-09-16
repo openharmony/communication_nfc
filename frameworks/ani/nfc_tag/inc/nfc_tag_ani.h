47ffe317edfe849b4f1216338caaffc3169fc926
y60072242 <yangpengwei5@h-partners.com>
2025-09-16 12:13:25 +0800

    解决xts无法读到常量问题
    Signed-off-by: y60072242 <yangpengwei5@h-partners.com>

--- frameworks/ani/nfc_tag/inc/nfc_tag_ani.h
+++ frameworks/ani/nfc_tag/inc/nfc_tag_ani.h
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2025-2025 Huawei Device Co., Ltd.
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,8 +13,9 @@
  * limitations under the License.
  */
 
-@!sts_inject("""
-static { loadLibrary("nfc_fwk_taihe_cardEmulation.z"); }
-""")
+#ifndef NFC_TAG_ANI_H
+#define NFC_TAG_ANI_H
 
-@!namespace("@ohos.nfc.cardEmulation", "nfcCardEmulation")
+#include <ani.h>
+
+#endif // NFC_TAG_ANI_H
