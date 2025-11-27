d32d8e99ac5b7c528d3ff858d718807d858663a4
y60072242 <yangpengwei5@h-partners.com>
2025-11-27 15:21:10 +0800

    解决恢复出厂设置场景下NFC不默认打开的问题
    Signed-off-by: y60072242 <yangpengwei5@h-partners.com>

--- services/src/nfc_sa_manager.cpp
+++ services/src/nfc_sa_manager.cpp
@@ -77,7 +77,7 @@ bool NfcSaManager::Init(const SystemAbilityOnDemandReason &startReason)
         nfcService_->Initialize();
         if (startReason.GetName() != "usual.event.BOOT_COMPLETED") {
             nfcService_->ExecuteTask(KITS::TASK_INITIALIZE);
-        } else if ((NfcParamUtil::GetNfcParamStr(NFC_DEFAULT_ON_PARAM_NAME) == "true") && 
+        } else if ((NfcParamUtil::GetNfcParamStr(NFC_DEFAULT_ON_PARAM_NAME) == "true") &&
             (NfcParamUtil::GetNfcParamStr(IS_FIRST_TIME_ENABLE_PARAM_NAME) == "true")) {
             nfcService_->ExecuteTask(KITS::TASK_INITIALIZE);
         }
