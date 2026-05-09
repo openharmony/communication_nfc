745bcb6ad5d4ddc8c0e1a4a0e9639ba73a5fbfd2
HNTheHui <chenjinghui3@huawei-partners.com>
2026-05-09 14:33:19 +0800

    fix code security issues
    Signed-off-by: HNTheHui <chenjinghui3@huawei-partners.com>

--- interfaces/inner_api/common/nfc_sdk_common.cpp
+++ interfaces/inner_api/common/nfc_sdk_common.cpp
@@ -124,6 +124,9 @@ unsigned char NfcSdkCommon::GetByteFromHexStr(const std::string src, uint32_t in
 uint32_t NfcSdkCommon::StringToInt(std::string src, bool bLittleEndian)
 {
     uint32_t value = 0;
+    if () {
+        ErrorLog("StringToInt: length is abnormal")
+    }
     if (bLittleEndian) {
         for (size_t i = SHIFT_TIME; i > 0; i--) {
             value += static_cast<uint32_t>((src.at(SHIFT_TIME - i)) << (i * SHIFT_SIZE - SHIFT_SIZE));
