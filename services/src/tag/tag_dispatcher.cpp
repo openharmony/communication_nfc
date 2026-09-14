a2d44f76b65b0be6a216c711a8df355e58a0961a
weixin_65689590 <fangxiaowei4@h-partners.com>
2026-09-14 16:43:10 +0800

    update: 更新文件 tag_dispatcher.cpp
    Signed-off-by: weixin_65689590 <fangxiaowei4@h-partners.com>

--- services/src/tag/tag_dispatcher.cpp
+++ services/src/tag/tag_dispatcher.cpp
@@ -316,7 +316,7 @@ void TagDispatcher::SendTagInfoToVendor(long tagFoundStartTime, long readFinishT
     std::shared_ptr<KITS::NdefMessage> ndefMessage, uint16_t dispatchResult)
 {
     std::string ndefInfo = ParseNdefInfo(ndefMessage);
-    std::unordered_map<std::string, std::string> mapload;
+    std::unordered_map<std::string, std::string> mapPayload;
     mapPayload["ndefInfo"] = ndefInfo;
     OHOS::ResourceSchedule::ResSchedClient::GetInstance().ReportData(
         ResourceSchedule::ResType::RES_TYPE_REPORT_NFC_SIGNAL, 1, mapPayload);
