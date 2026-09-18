4c234ef70416b49370651d3b84e95041707a3b92
wenxiaolin2 <wenxiaolin2@huawei.com>
2026-09-18 11:03:32 +0800

    代码规范修改
    Co-Authored-By:Agent
    Signed-off-by: wenxiaolin2 <wenxiaolin2@huawei.com>

--- interfaces/inner_api/controller/nfc_controller_callback_stub.cpp
+++ interfaces/inner_api/controller/nfc_controller_callback_stub.cpp
@@ -74,6 +74,7 @@ int NfcControllerCallBackStub::OnRemoteRequest(
 KITS::ErrorCode NfcControllerCallBackStub::RegisterCallBack(const sptr<INfcControllerCallback> &callBack)
 {
     DebugLog("NfcControllerCallBackStub RegisterCallBack");
+    std::unique_lock<std::shared_mutex> guard(callbackMutex);
     if (callback_ != nullptr) {
         ErrorLog("NfcControllerCallBackStub:Callback_ has registered!");
         return KITS::ERR_NFC_PARAMETERS;
